256f50f8836759d12a100424a28f4d2daad479e2
SYdney-Go <ksy960809@gmail.com>
2022-02-24 12:58:42 +0900

    20220214

--- 혼자공부하는C언어/Chapter2_상수와데이터출력/제어문자의출력.c
+++ 혼자공부하는C언어/Chapter2_상수와데이터출력/제어문자의출력.c
@@ -5,8 +5,8 @@ int main(void)
     printf("Be happy!\n");
     printf("12345678901234567890\n");
     printf("My\tfriend!\n");
-    printf("Goot\bd\tchance\n");
-    printf("Cow\rW\a\n");
+    printf("Goot\bd\tchance\n"); //\b는 한칸 왼쪽으로 삭제
+    printf("Cow\rW\a\n"); //\r은 맨 앞으로 이동한다. \a는 벨소리를 낸다.
 
     return 0;
 }
